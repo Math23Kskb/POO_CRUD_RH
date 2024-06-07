a145254ba82e6c0536f32cde8a670d2347056be6
MATHC <mathchiyuki@gmail.com>
2024-06-06 21:12:04 -0300

    Criacao de packages e Model -> Funcionario

--- Model/Funcionario.java
+++ Model/Funcionario.java
@@ -2,104 +2,128 @@ package Model;
 
 import java.util.Date;
 
-    public class Funcionario {
+public class Funcionario {
 
-        private int cod_func;
-        private String cpf;
-        private String primeiro_nome;
-        private String ultimo_nome;
-        private String endereco;
-        private Date data_nasc;
-        private int ddd;
-        private int telefone;
-        private int departamento_cod_dep;
+    private int codFunc;
+    private String cpf;
+    private String primeiroNome;
+    private String ultimoNome;
+    private String endereco;
+    private Date dataNasc;
+    private int ddd;
+    private int telefone;
+    private Departamento departamento;
 
-
-        public Funcionario() {
-        }
-
-        public Funcionario(String cpf, String primeiro_nome, String ultimo_nome, String endereco, Date data_nasc,
-                           int ddd, int telefone, int departamento_cod_dep) {
-            this.cpf = cpf;
-            this.primeiro_nome = primeiro_nome;
-            this.ultimo_nome = ultimo_nome;
-            this.endereco = endereco;
-            this.data_nasc = data_nasc;
-            this.ddd = ddd;
-            this.telefone = telefone;
-            this.departamento_cod_dep = departamento_cod_dep;
-        }
-
-        public Funcionario(int cod_func, String cpf, String primeiro_nome, String ultimo_nome, String endereco, Date data_nasc,
-                           int ddd, int telefone, int departamento_cod_dep) {
-            this.cod_func = cod_func;
-            this.cpf = cpf;
-            this.primeiro_nome = primeiro_nome;
-            this.ultimo_nome = ultimo_nome;
-            this.endereco = endereco;
-            this.data_nasc = data_nasc;
-            this.ddd = ddd;
-            this.telefone = telefone;
-            this.departamento_cod_dep = departamento_cod_dep;
-        }
-
-        public int getCod_func() {
-            return cod_func;
-        }
-        public String getCpf() {
-            return cpf;
-        }
-        public void setCpf(String cpf) {
-            this.cpf = cpf;
-        }
-        public String getPrimeiro_nome() {
-            return primeiro_nome;
-        }
-        public void setPrimeiro_nome(String primeiro_nome) {
-            this.primeiro_nome = primeiro_nome;
-        }
-        public String getUltimo_nome() {
-            return ultimo_nome;
-        }
-        public void setUltimo_nome(String ultimo_nome) {
-            this.ultimo_nome = ultimo_nome;
-        }
-        public String getEndereco() {
-            return endereco;
-        }
-        public void setEndereco(String endereco) {
-            this.endereco = endereco;
-        }
-        public Date getData_nasc() {
-            return data_nasc;
-        }
-        public void setData_nasc(Date data_nasc) {
-            this.data_nasc = data_nasc;
-        }
-        public int getDdd() {
-            return ddd;
-        }
-        public void setDdd(int ddd) {
-            this.ddd = ddd;
-        }
-        public int getTelefone() {
-            return telefone;
-        }
-        public void setTelefone(int telefone) {
-            this.telefone = telefone;
-        }
-        public int getDepartamento_cod_dep() {
-            return departamento_cod_dep;
-        }
-        public void setDepartamento_cod_dep(int departamento_cod_dep) {
-            this.departamento_cod_dep = departamento_cod_dep;
-        }
-
-        @Override
-        public String toString() {
-            return "Model.Funcionario [cod_func=" + cod_func + ", cpf=" + cpf + ", primeiro_nome=" + primeiro_nome
-                    + ", ultimo_nome=" + ultimo_nome + ", endereco=" + endereco + ", data_nasc=" + data_nasc + ", ddd="
-                    + ddd + ", telefone=" + telefone + ", departamento_cod_dep=" + departamento_cod_dep + "]";
-        }
+    // Construtor
+    public Funcionario() {
     }
 
+    public Funcionario(String cpf, String primeiroNome, String ultimoNome, String endereco, Date dataNasc,
+                       int ddd, int telefone, Departamento departamento) {
+        this.cpf = cpf;
+        this.primeiroNome = primeiroNome;
+        this.ultimoNome = ultimoNome;
+        this.endereco = endereco;
+        this.dataNasc = dataNasc;
+        this.ddd = ddd;
+        this.telefone = telefone;
+        this.departamento = departamento;
+    }
+
+    public Funcionario(int codFunc, String cpf, String primeiroNome, String ultimoNome, String endereco, Date dataNasc,
+                       int ddd, int telefone, Departamento departamento) {
+        this.codFunc = codFunc;
+        this.cpf = cpf;
+        this.primeiroNome = primeiroNome;
+        this.ultimoNome = ultimoNome;
+        this.endereco = endereco;
+        this.dataNasc = dataNasc;
+        this.ddd = ddd;
+        this.telefone = telefone;
+        this.departamento = departamento;
+    }
+
+    // Getters e Setters
+    public int getCodFunc() {
+        return codFunc;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getUltimoNome() {
+        return ultimoNome;
+    }
+
+    public void setUltimoNome(String ultimoNome) {
+        this.ultimoNome = ultimoNome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Date getDataNasc() {
+        return dataNasc;
+    }
+
+    public void setDataNasc(Date dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public int getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(int ddd) {
+        this.ddd = ddd;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "codFunc=" + codFunc +
+                ", cpf='" + cpf + '\'' +
+                ", primeiroNome='" + primeiroNome + '\'' +
+                ", ultimoNome='" + ultimoNome + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", dataNasc=" + dataNasc +
+                ", ddd=" + ddd +
+                ", telefone=" + telefone +
+                ", departamento=" + departamento +
+                '}';
+    }
+}
