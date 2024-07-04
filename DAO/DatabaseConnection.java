3140745d3100fab8becf240d39f4dcdc86f3a2bb
MATHC <mathchiyuki@gmail.com>
2024-07-04 16:36:53 -0300

    update DatabaseConnection User & Password
    Import mysql-connector-j-9.0.0.jar

--- DAO/DatabaseConnection.java
+++ DAO/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/mydb";
-    private static final String USER = "usuario";
-    private static final String PASSWORD = "senha";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() {
         try {
